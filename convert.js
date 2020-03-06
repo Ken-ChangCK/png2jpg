fec5970bee909552cfbcc677829610c9821842cc
chekai7 <45897779+chekai7@users.noreply.github.com>
2020-03-06 22:57:49 +0800

    123

--- convert.js
+++ convert.js
@@ -7,7 +7,7 @@ $("#progressbarTWInput").change(function(){
   const file = document.querySelector('#progressbarTWInput').files[0]
 	URL.createObjectURL(file)
   if (input.files && input.files.length >= 0) {
-    for(var i = 0; i < input.files.length; i ++){
+    for(var i = 0; i < input.files.length; i ++){ 
       var reader = new FileReader();
       reader.onload = function (e) {
        var img = $("<img width='300' height='200' margin-right='50px'>")
